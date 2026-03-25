9bf512ef452807f97d4b13f95679475c7a19edc8
VietHoang-88 <viethoanglor@gmail.com>
2026-03-26 02:26:09 +0700

    Cap nhat tai khoan Admin thanh hhkdigital

--- build.sh
+++ build.sh
@@ -6,3 +6,17 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Tự động tạo tài khoản Admin (Nếu chưa tồn tại)
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = 'hhkdigital'
+email = 'viethoanglor@gmail.com'
+password = 'hhkdigital'
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f'Superuser "{username}" created successfully!')
+else:
+    print(f'Superuser "{username}" already exists.')
+EOF
