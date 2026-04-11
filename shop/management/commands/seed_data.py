664e5e140c9cd7671a2cbf09665cb541052814a5
VietHoang-88 <viethoanglor@gmail.com>
2026-04-11 13:30:31 +0700

    Split Buy button into Add to Cart and Buy Now (Direct Checkout)

--- shop/management/commands/seed_data.py
+++ shop/management/commands/seed_data.py
@@ -25,14 +25,14 @@ class Command(BaseCommand):
                     'price': 29990000, 
                     'stock': 50, 
                     'description': 'Màn hình 6.7 inch, chip A17 Pro mạnh mẽ.',
-                    'image_url': 'https://vcdn1-sohoa.vnecdn.net/2023/09/13/iphone-15-pro-max-titan-xanh-1-3642-1694553313.jpg'
+                    'image_url': 'https://th.bing.com/th/id/OIP.rkgqD1ZsstYsu7Lzw1PIWQHaFQ?w=288&h=204&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
                 {
                     'name': 'Samsung Galaxy S24 Ultra', 
                     'price': 26990000, 
                     'stock': 40, 
                     'description': 'Bút S-Pen tích hợp, camera 200MP.',
-                    'image_url': 'https://images.samsung.com/is/image/samsung/p6pim/vn/2401/gallery/vn-galaxy-s24-s928-sm-s928bztqxxv-thumb-539311624'
+                    'image_url': 'https://th.bing.com/th/id/OIP.RCaOvi3C_QXx33EUvGht9wHaFj?w=245&h=184&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
             ],
             'Máy tính bảng': [
@@ -41,14 +41,14 @@ class Command(BaseCommand):
                     'price': 19990000, 
                     'stock': 30, 
                     'description': 'Chip M2 siêu mạnh, màn hình Liquid Retina.',
-                    'image_url': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-finish-select-202212-11inch-spacegray-wifi_AV1?wid=5120&hei=2880&fmt=p-jpg&qlt=95&.v=1670868635421'
+                    'image_url': 'https://th.bing.com/th/id/OIP.xMM2jSZArKHeWydu-uv38gHaHa?w=172&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
                 {
                     'name': 'Samsung Galaxy Tab S9', 
                     'price': 15490000, 
                     'stock': 25, 
                     'description': 'Màn hình Dynamic AMOLED 2X, kháng nước IP68.',
-                    'image_url': 'https://images.samsung.com/is/image/samsung/p6pim/vn/2307/gallery/vn-galaxy-tab-s9-wifi-sm-x710nzeaxxv-thumb-537443152'
+                    'image_url': 'https://th.bing.com/th/id/OIP.ELCbKzqe8iiurmk6ewaMgQHaFE?w=261&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
             ],
             'Laptop': [
@@ -57,14 +57,14 @@ class Command(BaseCommand):
                     'price': 27990000, 
                     'stock': 20, 
                     'description': 'Thiết kế siêu mỏng, hiệu năng vượt trội với chip M3.',
-                    'image_url': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-midnight-select-202403?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1707847253045'
+                    'image_url': 'https://th.bing.com/th/id/OIP.q9cWZbW4_eXQDyIa_07X9gHaFj?w=246&h=185&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
                 {
                     'name': 'Dell XPS 13 9340', 
                     'price': 34990000, 
                     'stock': 15, 
                     'description': 'Laptop cao cấp, màn hình vô cực OLED.',
-                    'image_url': 'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/xps-notebooks/xps-13-9340/media-gallery/touch-platinum/notebook-xps-13-9340-t-platinum-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3501&hei=2103&qlt=100,1&resMode=sharp2&size=3501,2103&chrss=full&imwidth=5000'
+                    'image_url': 'https://th.bing.com/th/id/OIP.-ScWbeEErL-wWE7PhDmGdQHaEK?w=260&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
             ],
             'Loa': [
@@ -89,7 +89,7 @@ class Command(BaseCommand):
                     'price': 9490000, 
                     'stock': 60, 
                     'description': 'Theo dõi sức khỏe, màn hình Always-On siêu sáng.',
-                    'image_url': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MT9G3_VW_34FR+watch-45-alum-midnight-nc-s9_VW_34FR_WF_CO?wid=750&hei=712&trim=1,0&fmt=p-jpg&qlt=95&.v=1694507905569'
+                    'image_url': 'https://th.bing.com/th/id/OIP.dQwi9KnaFg47d8sUFE79VAHaIs?w=175&h=206&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
                 {
                     'name': 'AirPods Pro (2nd generation) MagSafe', 
@@ -105,14 +105,14 @@ class Command(BaseCommand):
                     'price': 12990000, 
                     'stock': 15, 
                     'description': 'Màu sắc sống động, độ phân giải 4K sắc nét.',
-                    'image_url': 'https://images.samsung.com/is/image/samsung/p6pim/vn/ua55au7700kxxv/gallery/vn-uhd-au7000-ua55au7700kxxv-thumb-427210134'
+                    'image_url': 'https://th.bing.com/th/id/OIP.9NEw4KS_p6PC0U_IFzsztQHaHa?w=206&h=206&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
                 {
                     'name': 'Sony BRAVIA XR OLED 65 inch XR-65A80L', 
                     'price': 45990000, 
                     'stock': 10, 
                     'description': 'Màn hình OLED hoàn hảo, âm thanh phát ra từ màn hình.',
-                    'image_url': 'https://www.sony.com.vn/image/61001469e38f9725f4a7f0e633276632?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF'
+                    'image_url': 'https://th.bing.com/th/id/OIP.plxlcahm1jHNYEDvbkJm4wHaE0?w=325&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
                 },
             ],
         }
