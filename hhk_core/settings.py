ddcb01ffd160a0e9817eb2d98ab733c059e658a7
VietHoang-88 <viethoanglor@gmail.com>
2026-04-11 11:56:36 +0700

    Robust database configuration

--- hhk_core/settings.py
+++ hhk_core/settings.py
@@ -70,12 +70,15 @@ WSGI_APPLICATION = 'hhk_core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-        default=f'sqlite:///{ (BASE_DIR / "db.sqlite3").as_posix() }'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# Chỉ sử dụng DATABASE_URL nếu nó thực sự có giá trị
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
