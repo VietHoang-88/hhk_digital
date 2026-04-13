c036991c0ab1d2ca17d3a498116de644623da84d
VietHoang-88 <viethoanglor@gmail.com>
2026-04-13 22:08:30 +0700

    Optimize project: remove seed_data and clear_db commands

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate --noinput
-# Lệnh xóa sạch dữ liệu cũ bị lỗi ảnh (Lưu ý: Bạn nên xóa dòng này sau khi dữ liệu đã sạch)
-python manage.py clear_db
 
 # Tạo Superuser mặc định (tk/mk: hhkdigital)
 export DJANGO_SUPERUSER_USERNAME=hhkdigital
