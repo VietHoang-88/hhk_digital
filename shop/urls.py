4ee4f696a8ed5ba27c1d7b0f827cf7678bd92e03
VietHoang-88 <viethoanglor@gmail.com>
2026-04-15 13:19:41 +0700

    Fix logout redirect to use view name instead of URL path

--- shop/urls.py
+++ shop/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='shop/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='shop:product_list'), name='logout'),
     path('register/', views.register, name='register'),
 ]
